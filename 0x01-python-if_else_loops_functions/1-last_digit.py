f8575e5f9799fb4c4620648321bbf70ac6a11b6f
Right Praise <rightpraise@gmail.com>
2022-11-29 18:56:39 -0800

    Adds format to print statement

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,8 +9,8 @@ if x == '0':
 else:
     if number > 0:
         if int(x) > 5:
-            print("Last digit of {num} is {x} and is greater than 5")
+            print(f"Last digit of {num} is {x} and is greater than 5")
         elif int(x) < 6 and x != 0:
-            print("Last digit of {num} is {x} and is less than 6 and not 0")
+            print(f"Last digit of {num} is {x} and is less than 6 and not 0")
     else:
-        print("Last digit of {num} is -{x} and is less than 6 and not 0")
+        print(f"Last digit of {num} is -{x} and is less than 6 and not 0")
