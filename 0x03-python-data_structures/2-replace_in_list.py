63cfea24d60666fc737ffb4150d0a66919c5fd2d
Right Praise <rightpraise@gmail.com>
2022-12-05 08:36:56 -0800

    Corrects code

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,5 +6,5 @@ def replace_in_list(my_list, idx, element):
         if idx > len(my_list):
             return my_list
         elif idx == x:
-            my_list[x] = element
+            my_list[idx] = element
             return my_list
