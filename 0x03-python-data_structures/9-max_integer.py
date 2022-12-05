2545ff92a30be2256eb6ed4a9a0198ecbda9e885
Right Praise <rightpraise@gmail.com>
2022-12-05 13:04:54 -0800

    Corrects code

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,6 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    else:
-        my_list.sort()
-        return my_list[-1]
+    my_list.sort()
+    return my_list[-1]
 
