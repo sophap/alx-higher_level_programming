0e9cf94a4b48d725de41714b4d0858805386a929
Right Praise <rightpraise@gmail.com>
2022-12-05 12:51:17 -0800

    Modifies code

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    for x in range(len(sentence)):
-        if len(sentence) == 0:
-            result = (len(sentence), None)
-        else:
-            result = (len(sentence), sentence[0])
+    if sentence == '':
+        result = (len(sentence), None)
+    else:
+        result = (len(sentence), sentence[0])
     return result
