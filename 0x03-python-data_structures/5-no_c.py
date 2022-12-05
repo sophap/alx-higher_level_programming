fbdca527b8c0bc5db7c5ab15134ecc8d316b8c71
Right Praise <rightpraise@gmail.com>
2022-12-05 11:28:39 -0800

    Modifies code

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for x in range(len(my_string) - 2):
+    for x in range(len(my_string)):
         if my_string[x] == 'c' or my_string[x] == 'C':
             my_string = my_string[:x] + "" + my_string[x + 1:]
     return my_string
