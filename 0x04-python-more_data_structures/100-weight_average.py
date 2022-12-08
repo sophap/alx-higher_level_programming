044fc119609d3c86a2489399693d15c1373b8f9e
Right Praise <rightpraise@gmail.com>
2022-12-07 19:05:34 -0800

    Fixes code

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list == [] or my_list is Noone:
+    if my_list == [] or my_list is None:
         return 0
     sum = total = 0
     for (a, b) in my_list:
