65f3e0f0db6feb4e3c4d77b02912d3865be2b10a
Right Praise <rightpraise@gmail.com>
2023-01-09 14:15:12 -0800

    Corrects code

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,9 +1,7 @@
-#!/usr/bin/python3                                                                                                                                                                            
-"""                                                                                                                                                                                           
-Base geometry class                   
-"""                                                                                                                                                                             
-                                                                                                                                                                                             
-                                                                                                                                                                                             
+#!/usr/bin/python3
+""" Base Geometry Class """
+
+
 class BaseGeometry:
     """ class that improve geometry with integer validator"""
     def area(self):
@@ -15,19 +13,19 @@ class BaseGeometry:
         if (type(value) is not int):
             raise TypeError(name + " must be an integer")
         if (value <= 0):
-            raise ValueError(name + " must be greater than 0")                                                                                                                                                                                              
-                                                                                                                                                                                              
-"""Program that builds a rectangle"""                                                                                                                               
-                                                                                                                                                                                              
-                                                                                                                                                                                              
-class Rectangle(BaseGeometry):                                                                                                                                                                
-    """ class Rectangle that inherits from BaseGeometry"""                                                                                                                                    
-    def __init__(self, width, height):                                                                                                                                                        
-        """ Constructor"""                                                                                                                                                                    
-        self.__width = width                                                                                                                                                                  
-        self.__height = height                                                                                                                                                                
-        BaseGeometry.integer_validator(self, "width", self.__width)                                                                                                                           
-	BaseGeometry.integer_validator(self, "height", self.__height)
+            raise ValueError(name + " must be greater than 0")
+
+""" Program that build a full rectangle """
+
+
+class Rectangle(BaseGeometry):
+    """ class Rectangle that inherits from BaseGeometry """
+    def __init__(self, width, height):
+        """ Constructor """
+        self.__width = width
+        self.__height = height
+        BaseGeometry.integer_validator(self, "width", self.__width)
+        BaseGeometry.integer_validator(self, "height", self.__height)
 
     def area(self):
         """ method that return de area of rectangle """
