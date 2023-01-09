7eb92512f7b540a5bb8557caa2eff13900d7658f
Right Praise <rightpraise@gmail.com>
2023-01-09 09:49:25 -0800

    Inherits from class list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module of inherited list
+"""
+
+Class Mylist(list):
+    """
+    My list
+    """
+    def print_sorted(self):
+        """
+        Prints sorted list
+        """
+        print(sorted(self))
