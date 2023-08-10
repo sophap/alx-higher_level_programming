3a8a6a1c1b4470422b54ed7997788bc8226c8bc4
Right Praise <rightpraise@gmail.com>
2023-08-10 13:34:31 -0700

    fixes code

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-import sys
-def add(argv):
-    sum = 0
-    for arg in argv:
-        sum += int(arg)
-    return sum
 if __name__ == "__main__":
-    result = add(sys.argv[1:])
-    print(result)
+    import sys
+    sum = 0
+    for arg in sys.argv[1:]:
+        sum += int(arg)
+    print("{:d}".format(sum))
