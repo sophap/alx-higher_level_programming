7a770e90a92bcba6a71d92554784c3dc7e900063
Right Praise <rightpraise@gmail.com>
2023-08-10 14:07:41 -0700

    fixes code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,7 @@
 if __name__ == "__main__":
     import sys
     num_args = len(sys.argv[1:])
-    print("{:d} argument{}{}".format(num_args,
-        "s" if num_args != 1 else "",
-        "." if num_args == 0 else ":"))
+    print("{:d} argument{}{}".format(num_args, "s" if num_args != 1 else "",
+                                     "." if num_args == 0 else ":"))
     for x, args in enumerate(sys.argv[1:]):
         print("{}: {}".format(x + 1, args))
