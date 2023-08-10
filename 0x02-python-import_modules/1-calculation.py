71549cbbb80065eb5ea29cb96e6caac686d65fa6
Right Praise <rightpraise@gmail.com>
2023-08-10 12:50:52 -0700

    fixes code

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-a = 10
-b = 5
-from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    from calculator_1 import add, sub, mul, div
 
-sum = add(a, b)
-diff = sub(a, b)
-prod = mul(a, b)
-quot = div(a, b)
+    sum = add(a, b)
+    diff = sub(a, b)
+    prod = mul(a, b)
+    quot = div(a, b)
 
-print("{} + {} = {}".format(a, b, sum))
-print("{} - {} = {}".format(a, b, diff))
-print("{} * {} = {}".format(a, b, prod))
-print("{} / {} = {}".format(a, b, quot))
+    print("{} + {} = {}".format(a, b, sum))
+    print("{} - {} = {}".format(a, b, diff))
+    print("{} * {} = {}".format(a, b, prod))
+    print("{} / {} = {}".format(a, b, quot))
