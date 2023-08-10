f6e3d1ba3fc99d0098099e32c2f00a9e1ab6b6c1
Right Praise <rightpraise@gmail.com>
2023-08-10 13:27:46 -0700

    fixes code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-def bine():
+if __name__ == "__main__":
+    import hidden_4
     for x in dir(hidden_4):
         if not (x[0] == "_" and x[1] == "_"):
             print(x)
-if __name__ == "__main__":
-    bine()
