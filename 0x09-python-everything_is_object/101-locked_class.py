5cbad71f2250df258701ebd3e88aa0e6f87e308a
Right Praise <rightpraise@gmail.com>
2023-09-15 05:32:06 -0700

    prevents the user from dynamically creating new instance attributes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+This is the module docstring. It provides an overview of the module's purpose.
+"""
+
+
 class LockedClass:
+    """
+    This is the docstring for the LockedClass.
+    It explains the purpose of the class and how it works.
+    """
+
     def __setattr__(self, attr, value):
         if attr != 'first_name':
             raise AttributeError("'LockedClass' object has no attribute '{}'".format(attr))
